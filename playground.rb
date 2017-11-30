8a25f913e66db4dbd71e365542cc99c6f2e22bf0
shakusbakus <hit_or_miss20032004@yahoo.com>
2017-11-30 21:33:06 +0000

    playground shortened

--- playground.rb
+++ playground.rb
@@ -6,9 +6,5 @@ last_name = gets.chomp
 
 puts " #{first_name} #{last_name}, you are now a Racker"
 
-first_name_length = first_name.length
-last_name_length = last_name.length
-
-puts "Your first name is #{first_name_length} characters long and your surname #{last_name_length} characters long so well done!" 
-
+puts "Your first name is #{first_name.length} characters long and your surname #{last_name.length} characters long so well done!" 
 
